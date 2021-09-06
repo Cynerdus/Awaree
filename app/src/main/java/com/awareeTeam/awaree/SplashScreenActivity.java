55ac268fcf1c4a099b76df00f56b302fa279ebae
Cynerdus <melody.nickname@gmail.com>
2021-09-06 22:34:21 +0300

    Added SplashScreen and played a bit with the .xmls

---
+++ app/src/main/java/com/awareeTeam/awaree/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.awareeTeam.awaree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
