bf725b87191833d3765f301d646c20ca804e0e80
BuildTools <unconfigured@null.spigotmc.org>
2021-09-10 21:11:52 +0300

    Fixed crash

--- app/src/main/java/com/awareeTeam/awaree/DashboardFragment.java
+++ app/src/main/java/com/awareeTeam/awaree/DashboardFragment.java
@@ -16,4 +16,7 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
+
 }
